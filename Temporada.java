62182b2dda3303dd19bacad078e7a58a61ee8ea6
Myrian <myrianhf95@gmail.com>
2021-11-04 14:09:38 +0100

    01. Creación clase temporada, atributos y constructor

---
+++ Temporada.java
@@ -0,0 +1,15 @@
+public class Temporada {
+    
+    // Número de capítulos
+    private int numeroCapitulos;
+    // Serie terminada
+    private boolean serieTerminada;
+
+    /**
+     * Crea estructura de la temporada
+     */
+    public Temporada(int numeroCapitulosTemporada, boolean serieFinalizada) {
+        numeroCapitulos = numeroCapitulosTemporada;
+        serieTerminada = serieFinalizada;
+    }
+}
